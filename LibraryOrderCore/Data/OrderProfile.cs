df07293ce50ad5e97c06ede6b00f2af05aa18759
maxime.zegres <maxime.zegres@gmail.com>
2019-03-07 21:59:47 +0100

    Add OrderProfile(configuration AutoMapper)

---
+++ LibraryOrderCore/Data/OrderProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using LibraryOrderCore.Data.Entities;
+using LibraryOrderCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryOrderCore.Data
+{
+    public class OrderProfile : Profile
+    {
+        public OrderProfile()
+        {
+            this.CreateMap<Order, OrderModel>();
+        }
+    }
+}
