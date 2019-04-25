95f23caec810f8c92cdf656c063ed89ce353f39f
maxime.zegres <maxime.zegres@gmail.com>
2019-04-25 05:31:26 +0200

    Reset Form to use the TemplateDrivenForm

--- LibraryOrderClientApp/src/app/add-order/add-order.component.ts
+++ LibraryOrderClientApp/src/app/add-order/add-order.component.ts
@@ -8,56 +8,13 @@ import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@ang
   styleUrls: ['./add-order.component.css']
 })
 export class AddOrderComponent implements OnInit {
-  orderForm: FormGroup
 
-  constructor(private router: Router, private fb: FormBuilder) {   
+  constructor(private router: Router) {   
   }
-
-  get orderItems(): FormArray {
-    return this.orderForm.get('orderItems') as FormArray;
-  }
-
 
   ngOnInit() {
-    this.orderForm = this.fb.group({
-      orderNumber: ['', [Validators.required]],
-      orderDate: ['', [Validators.required]],
-      isContacted: [false, [Validators.required]],
-      customerFirstName: ['', [Validators.required]],
-      customerLastName : ['', [Validators.required]],
-      customerEmail: ['', [Validators.required]],
-      customerPhoneNumber: ['', [Validators.required]],
-      orderItems: this.fb.array([ this.buildBooks() ])
-    });
+    
   }
-
-    addBooks() : void {
-        this.orderItems.push(this.buildBooks());
-    }
-
-
-    buildBooks() {
-      return this.fb.group({
-        quantity: ['', [Validators.required]],
-        isOrdered: [false, [Validators.required]],
-        book: this.fb.group({
-          title: ['', [Validators.required]],
-          author: ['', [Validators.required]],
-          editor: ['', [Validators.required]],
-          isbn: ['', [Validators.required]]
-        })
-      });
-    }
   
-
-  saveCustomer() {
-    console.log(this.orderForm);
-    console.log('Saved: ' + JSON.stringify(this.orderForm.value));
-  }
-
-  cancel() {
-    this.router.navigate(['/orders'])
-  }
-
 
 }
